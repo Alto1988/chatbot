c01b5e389f700ba11ee193d80a7c4c908197acfd
Alto1988 <jpagan5@neiu.edu>
2021-05-16 21:17:08 -0500

    Done for the weekend

---
+++ chatbot/test_settings.py
@@ -0,0 +1,8 @@
+from .settings import *
+
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+
+DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
--- slugs/tests/test_models.py
+++ slugs/tests/test_models.py
@@ -1,12 +1,14 @@
+from slugs.models import Messages
 import pytest
 from mixer.backend.django import mixer
+
 
 pytestmark = pytest.mark.django_db
 
 
-def test_message_created():
-    obj = mixer.blend('slugs.Messages')
-    assert obj.message is not None
+# def test_message_created():
+#     obj = mixer.blend('slugs.Messages')
+#     assert obj.message is not None
 
 
 # def test_message_fail():
@@ -22,3 +24,15 @@ def test_groups_created():
 def test_module_created():
     obj = mixer.blend('slugs.Module')
     assert obj.pk is not None
+
+
+class TestModels:
+    def test_message_create(self):
+        message = Messages.objects.create(
+            message="test Message",
+            title="test Title",
+        )
+
+    def test_model_display_data(self):
+        message = mixer.blend(Messages, title="Testing")
+        assert str(message) == 'Testing'
